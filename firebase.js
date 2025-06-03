f21c4a4686b8eb80fe32204400e7747e40153374
Ganesh Abhange <ganesh61980@gmail.com>
2025-06-03 23:08:10 +0530

    Removed Keys

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY, //"AIzaSyBRdMUe2HcPqDTaMLwA77wuurnDGo21E98",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN, //"caffiend-b8da7.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID, //"caffiend-b8da7",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET, //"caffiend-b8da7.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID, //"866149226766",
-  appId: import.meta.env.VITE_FIREBASE_APPID //"1:866149226766:web:890257e341995ae638df5f"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY, 
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN, 
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID, 
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET, 
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID, 
+  appId: import.meta.env.VITE_FIREBASE_APPID 
 };
 
 // Initialize Firebase
